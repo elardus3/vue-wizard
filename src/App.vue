d8b6685c2b6909d6dbddac9896b6b0e973f149f5
Larry <larry@example.com>
2025-03-06 10:45:54 +0200

    bun create vue@latest

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Menu from './components/Menu.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Menu />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/components/Menu.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Houses</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  text-align: center;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+}
+</style>
