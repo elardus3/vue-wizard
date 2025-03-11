33e0baafedc38c6ae6e8c9fe6fb74c9fd5d04606
Larry <larry@example.com>
2025-03-11 08:54:00 +0200

    define login store

---
+++ src/stores/login.ts
@@ -0,0 +1,26 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useLoginStore = defineStore('counter', () => {
+  const loggedIn = ref(false);
+  const admin = ref(false);
+
+  const isLoggedIn = computed(() => loggedIn.value === true);
+  const isAdmin = computed(() => admin.value === true);
+
+  function login(user: string, pass: string) {
+    if (user === 'a' && pass === 'a') {
+      loggedIn.value = true;
+      admin.value = true;
+    } else if (user === 'e' && pass === 'e') {
+      loggedIn.value = true;
+    }
+  }
+
+  function logout() {
+    loggedIn.value = false;
+    admin.value = false;
+  }
+
+  return { loggedIn, admin, isLoggedIn, isAdmin, login, logout }
+})
